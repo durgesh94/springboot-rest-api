c0800ef42ed7eada7bc8736d4ad6f1f4c46e813a
Durgesh Tambe <dntambe@gmail.com>
2026-08-01 14:28:19 +0530

    quick startup listner

---
+++ src/main/java/com/learning/springboot/listener/StartupListener.java
@@ -0,0 +1,21 @@
+package com.learning.springboot.listener;
+
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StartupListener {
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void onApplicationReady() {
+
+        System.out.println("""
+                
+                ==============================================
+                 🚀 Spring Boot REST API Started Successfully!
+                 🌐 URL : http://localhost:8080
+                ==============================================
+                """);
+    }
+}
