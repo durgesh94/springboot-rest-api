a0bac01567cbd956a8a6fd807e2aa12488d87dc0
Durgesh Tambe <dntambe@gmail.com>
2026-08-13 20:50:59 +0530

    step 3: product req/resp dto

---
+++ src/main/java/com/learning/springboot/product/dto/ProductRequestDto.java
@@ -0,0 +1,32 @@
+package com.learning.springboot.product.dto;
+
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ProductRequestDto {
+
+    @NotBlank(message = "Product name is required")
+    private String name;
+
+    @NotBlank(message = "Product description is required")
+    private String description;
+
+    @NotNull(message = "Product price is required")
+    @DecimalMin(value = "0.01", message = "Product price must be greater than 0")
+    private BigDecimal price;
+
+    @NotNull(message = "Product quantity is required")
+    @Min(value = 0, message = "Stock quantity cannot be negative")
+    private Integer quantity;
+    
+}
