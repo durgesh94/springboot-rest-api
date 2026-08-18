3b7735c13f65c8e6810d36f7b0f45c402506592f
Durgesh Tambe <dntambe@gmail.com>
2026-08-18 19:46:26 +0530

    feat: convert OrderItemRepository and OrderRepository to interfaces extending JpaRepository

--- src/main/java/com/learning/springboot/modules/order/repository/OrderItemRepository.java
+++ src/main/java/com/learning/springboot/modules/order/repository/OrderItemRepository.java
@@ -1,4 +1,7 @@
 package com.learning.springboot.modules.order.repository;
 
-public class OrderItemRepository {
+import com.learning.springboot.modules.order.entity.OrderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
 }
