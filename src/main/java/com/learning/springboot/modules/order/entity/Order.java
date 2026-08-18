3b562effba73da4d1b6018c9e7ed7f3517baca18
Durgesh Tambe <dntambe@gmail.com>
2026-08-18 19:40:02 +0530

    feat: implement Order and OrderItem entities with JPA annotations and relationships

--- src/main/java/com/learning/springboot/modules/order/entity/Order.java
+++ src/main/java/com/learning/springboot/modules/order/entity/Order.java
@@ -1,4 +1,35 @@
 package com.learning.springboot.modules.order.entity;
 
-public class Order {
+import com.learning.springboot.common.BaseEntity;
+import com.learning.springboot.modules.user.entity.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Order extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Builder.Default
+    @OneToMany(
+            mappedBy = "order",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true
+    )
+    private List<OrderItem> items = new ArrayList<>();
 }
