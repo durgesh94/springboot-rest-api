aab000cbc41570b9dc6bdaf1cecd8503f28e5377
Durgesh Tambe <dntambe@gmail.com>
2026-08-19 10:40:49 +0530

    feat: implement createOrder endpoint in OrderController for order creation

--- src/main/java/com/learning/springboot/modules/order/controller/OrderController.java
+++ src/main/java/com/learning/springboot/modules/order/controller/OrderController.java
@@ -1,4 +1,27 @@
 package com.learning.springboot.modules.order.controller;
 
+import com.learning.springboot.modules.order.dto.OrderRequestDto;
+import com.learning.springboot.modules.order.dto.OrderResponseDto;
+import com.learning.springboot.modules.order.service.OrderService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/orders")
+@RequiredArgsConstructor
 public class OrderController {
+
+    private final OrderService orderService;
+
+    @PostMapping
+    public ResponseEntity<OrderResponseDto> createOrder(@Valid @RequestBody OrderRequestDto orderRequest) {
+        OrderResponseDto orderResponse = orderService.createOrder(orderRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body(orderResponse);
+    }
 }
